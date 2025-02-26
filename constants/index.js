477c6f49635c1c7af2b9f84fac946116ad363260
jagwanic2001 <150118610+jagwanic2001@users.noreply.github.com>
2025-02-26 15:18:10 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,75 +1,61 @@
+import Link from "next/link";
+
 export const exploreWorlds = [
   {
     id: 'world-1',
     imgUrl: '/planet-01.png',
-    title: 'The Hogwarts',
+    title: 'TechWorld',
+    
+  
   },
   {
     id: 'world-2',
     imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    title: 'The Simply fascinating',
   },
   {
     id: 'world-3',
     imgUrl: '/planet-03.png',
-    title: 'Kadirojo Permai',
+    title: 'Breaking the norms',
   },
   {
     id: 'world-4',
     imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
+    title: 'Breaking the norms',
   },
   {
     id: 'world-5',
     imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
+    title: 'ways to achieve more',
+  
   },
 ];
 
 export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
-  'No need to beat around the bush, just stay on the gas and have fun',
+  'enter the world of kovon to find more things'
 ];
 
 export const newFeatures = [
   {
     imgUrl: '/vrpano.svg',
     title: 'A new world',
-    subtitle:
-        'we have the latest update with new world for you to try never mind',
+   
   },
   {
     imgUrl: '/headset.svg',
-    title: 'More realistic',
-    subtitle:
-        'In the latest update, your eyes are narrow, making the world more realistic than ever',
-  },
+    title: 'Tech experts',
+  
+  }
 ];
 
 export const insights = [
   {
-    imgUrl: '/planet-06.png',
-    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
-    subtitle:
-        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
-  },
-  {
-    imgUrl: '/planet-07.png',
-    title: '7 tips to easily master the madness of the Metaverse',
-    subtitle:
-        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
-  },
-  {
-    imgUrl: '/planet-08.png',
-    title: 'With one platform you can explore the whole world virtually',
-    subtitle:
-        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
-  },
+  }
+    
 ];
 
 export const socials = [
-  {
+  { id:'hi',
     name: 'twitter',
     url: '/twitter.svg',
   },
@@ -83,6 +69,6 @@ export const socials = [
   },
   {
     name: 'facebook',
-    url: '/facebook.svg',
+    url:'/facebook.svg'
   },
 ];
