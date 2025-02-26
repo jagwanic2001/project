69bce0dd8fd68f53b0ef2d873240908d25aeca0c
jagwanic2001 <150118610+jagwanic2001@users.noreply.github.com>
2025-02-26 15:10:20 +0530

    Update ExploreCard.jsx

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -8,15 +8,14 @@ import { fadeIn } from '../utils/motion';
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-    className={`relative ${
-      active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
-    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    className={`relative ${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
+      } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
     <img
-      src={imgUrl}
-      alt="planet-04"
-      className="absolute w-full h-full object-cover rounded-[24px]"
+      id='plugin2'
+      alt='planet-04'
+      className='absolute w-full h-full object-cover rounded-[24px]'
     />
     {active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
@@ -34,7 +33,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
           />
         </div>
         <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
-          Enter Metaverse
+          <a href='https://www.youtube.com/watch?v=ugCN_gynFYw'>click here</a>
         </p>
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
           {title}
